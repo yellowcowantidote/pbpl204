ce468f4d5b25573c06dfe4d2896420976fbdf5f0
AnGWar-26 <andrew.warfield@protonmail.com>
2020-06-02 21:16:11 -0700

    Update projectfunctions for new files - Modify import function to appropriately clean files for visualization with hvplot,geoviews - Update LQs calc and list generations TODO: update graph_codes for use with hvplot or geoviews

--- notebooks/projectfunctions.py
+++ notebooks/projectfunctions.py
@@ -5,8 +5,6 @@
 # %%
 
 
-import gzip
-import csv
 import pandas as pd
 import geopandas as gpd
 import numpy as np
@@ -16,82 +14,72 @@ import segregation
 from splot._viz_utils import shift_colormap
 from segregation.local import MultiLocationQuotient, MultiLocalDiversity, MultiLocalEntropy, MultiLocalSimpsonInteraction, MultiLocalSimpsonConcentration, LocalRelativeCentralization
 
-def cleanlink_wac(file,fips,shp):
-    """Automates the process of cleaning and linking the LODES data. Takes file(string), state + county fips(string),
-       and shp(string)as inputs. shp needs double / as it uses gpd instead of pd.
+def prep_data(file):
+    """Takes json as input. Reads, cleans, changes crs and sets index to make ready for visualization. Takes the file path as an argument(string).
     """
-    #read lodes data
-    lehd_df = pd.read_csv(file,compression='gzip',header=0,sep=',',quotechar='"',error_bad_lines=False)
-    #convert w_geocode to string to make it easier to check
-    lehd_df['w_geocode'] = lehd_df['w_geocode'].astype(str)
-    # check if beginning of string matches SD county fips code
-    lehd_df = lehd_df[lehd_df['w_geocode'].str.match(fips)]
-    #add leading 0 to make consistent with block data(below)
-    lehd_df['w_geocode'] = '0' + lehd_df['w_geocode']
-    lehd_df.rename(columns={'w_geocode': 'GEOID10'}, inplace=True)
-
-    blocks = gpd.read_file(shp)
-    blocks = blocks.merge(lehd_df, how='left', on='GEOID10')
-    try:
-        blocks.drop(['INTPTLON10','INTPTLAT10','COUNTYFP10','STATEFP10','NAME10','TRACTCE10','BLOCKCE10','MTFCC10','FUNCSTAT10','UR10','AWATER10','UACE10','UATYP10','createdate'], axis=1, inplace=True)
-    except:
-        blocks.drop(['INTPTLON10','INTPTLAT10','COUNTYFP10','STATEFP10','NAME10','TRACTCE10','BLOCKCE10','MTFCC10','FUNCSTAT10','UR10','AWATER10','UACE10','createdate'], axis=1, inplace=True)
-    blocks.fillna(value=0,inplace=True)
-    #Remove any blocks where there is no land area, no jobs there so why would we care about them?
-    blocks = blocks[blocks.ALAND10 != 0]
-    return blocks
+    df = gpd.read_file(file)
+    #Let's rearrange the columns so that we can more easily create lists to calc LQs
+    df = df[['labels', 'year', 'CFA01', 'CFA02', 'CFA03', 'CFA04', 'CFA05',
+       'CFS01', 'CFS02', 'CFS03', 'CFS04', 'CFS05', 'create_date',
+       'earnings_under_1250', 'earnings_1251_3333', 'earnings_over_3333',
+       'education_lths','education_hs', 'education_some_college','education_bachelors', 
+       'employees_under_30', 'employees_30_54', 'employees_55plus',
+       'employees_asian', 'employees_black',
+       'employees_hawaiian_pi', 'employees_hispanic',
+       'employees_native_american', 'employees_not_hispanic',
+       'employees_twoplus_races', 'employees_white', 'employees_male', 'employees_female',
+       'housing_units', 'naics_11', 'naics_21', 'naics_22', 'naics_23',
+       'naics_31_33', 'naics_42', 'naics_44_45', 'naics_48_89',
+       'naics_51', 'naics_52', 'naics_53', 'naics_54', 'naics_55',
+       'naics_56', 'naics_61', 'naics_62', 'naics_71', 'naics_72',
+       'naics_81', 'naics_90', 'population', 'total_employees',
+       'node_ids', 'emp', 'geometry']]
+    #lets drop fields we dont care about
+    df.drop(['create_date'], axis=1, inplace=True)
+    #get ready for visualization
+    df['label'] = df.index
+    df = df.to_crs(epsg=3857)
+    return df
 
 def generate_lists(dataframe):
     df_columns = dataframe.columns.tolist()
-    workerage_list = df_columns[4:7]
-    workerwage_list = df_columns[7:10]
-    jobs_list = df_columns[10:30]
-    race_list = df_columns[30:38]
-    edu_list= df_columns[38:42]
-    sex_list = df_columns[42:44]
-    firmage_list = df_columns[44:49]
-    firmsize_list = df_columns[49:54]
-    return(workerage_list,workerwage_list,jobs_list,race_list,edu_list,sex_list,firmage_list,firmsize_list)
+    earnings_list = df_columns[12:15]
+    edu_list= df_columns[15:19]
+    workerage_list = df_columns[19:22]
+    race_list = df_columns[22:30]
+    sex_list = df_columns[30:32]
+    jobs_list = df_columns[33:53]
+    return(workerage_list,earnings_list,jobs_list,race_list,edu_list,sex_list)
 
 def gen_workerage(dataframe):
     df_columns = dataframe.columns.tolist()
-    workerage_list = df_columns[4:7]
+    workerage_list = df_columns[19:22]
     return(workerage_list)
 
-def gen_workerwage(dataframe):
+def gen_earnings(dataframe):
     df_columns = dataframe.columns.tolist()
-    workerwage_list = df_columns[7:10]
-    return(workerwage_list)
+    earnings_list = df_columns[12:15]
+    return(earnings_list)
 
 def gen_jobs(dataframe):
     df_columns = dataframe.columns.tolist()
-    jobs_list = df_columns[10:30]
+    jobs_list = df_columns[33:53]
     return(jobs_list)
 
 def gen_race(dataframe):
     df_columns = dataframe.columns.tolist()
-    race_list = df_columns[30:38]
+    race_list = df_columns[22:30]
     return(race_list)
 
 def gen_edu(dataframe):
     df_columns = dataframe.columns.tolist()
-    edu_list = df_columns[38:42]
+    edu_list = df_columns[15:19]
     return(edu_list)
 
 def gen_sex(dataframe):
     df_columns = dataframe.columns.tolist()
-    sex_list = df_columns[42:44]
+    sex_list = df_columns[30:32]
     return(sex_list)
-
-def gen_firmage(dataframe):
-    df_columns = dataframe.columns.tolist()
-    firmage_list = df_columns[44:49]
-    return(firmage_list)
-
-def gen_firmsize(dataframe):
-    df_columns = dataframe.columns.tolist()
-    firmsize_list = df_columns[49:54]
-    return(firmsize_list)
 
 def calc_lq(dataframe):
     """
@@ -107,30 +95,25 @@ def calc_lq(dataframe):
     """
 
     df_columns = dataframe.columns.tolist()
-    workerage_list = df_columns[4:7]
-    workerwage_list = df_columns[7:10]
-    jobs_list = df_columns[10:30]
-    race_list = df_columns[30:38]
-    edu_list= df_columns[38:42]
-    sex_list = df_columns[42:44]
-    firmage_list = df_columns[44:49]
-    firmsize_list = df_columns[49:54]
+    earnings_list = df_columns[12:15]
+    edu_list= df_columns[15:19]
+    workerage_list = df_columns[19:22]
+    race_list = df_columns[22:30]
+    sex_list = df_columns[30:32]
+    jobs_list = df_columns[33:53]
 
     WorkerAgeLQIndex = MultiLocationQuotient(dataframe, workerage_list)
-    WorkerWageLQIndex = MultiLocationQuotient(dataframe, workerwage_list)
+    EarningsLQIndex = MultiLocationQuotient(dataframe, earnings_list)
     JobsLQIndex = MultiLocationQuotient(dataframe, jobs_list)
     RaceLQIndex = MultiLocationQuotient(dataframe, race_list)
     EduLQIndex = MultiLocationQuotient(dataframe, edu_list)
     SexLQIndex = MultiLocationQuotient(dataframe, sex_list)
-    FirmAgeLQIndex = MultiLocationQuotient(dataframe, firmage_list)
-    FirmSizeLQIndex = MultiLocationQuotient(dataframe, firmsize_list)
-
 
     for items in range(len(workerage_list)):
-        dataframe['LQ_' + workerage_list[items]] = WorkerWageLQIndex.statistics[:,items]
+        dataframe['LQ_' + workerage_list[items]] = WorkerAgeLQIndex.statistics[:,items]
 
-    for items in range(len(workerwage_list)):
-        dataframe['LQ_' + workerwage_list[items]] = WorkerWageLQIndex.statistics[:,items]
+    for items in range(len(earnings_list)):
+        dataframe['LQ_' + earnings_list[items]] = EarningsLQIndex.statistics[:,items]
 
     for items in range(len(jobs_list)):
         dataframe['LQ_' + jobs_list[items]] = JobsLQIndex.statistics[:,items]
@@ -143,30 +126,17 @@ def calc_lq(dataframe):
     
     for items in range(len(sex_list)):
         dataframe['LQ_' + sex_list[items]] = SexLQIndex.statistics[:,items]
-    
-    for items in range(len(firmage_list)):
-        dataframe['LQ_' + firmage_list[items]] = FirmAgeLQIndex.statistics[:,items]
-    
-    for items in range(len(firmsize_list)):
-        dataframe['LQ_' + firmsize_list[items]] = FirmSizeLQIndex.statistics[:,items]
 
 def graph_codes(dataframe,code,cmap='OrRd',k=5,scheme='fisherjenkssampled'):
     """A function that takes the dataframe, LEHD code(string), cmap(string), and k(int) as arguements. Graphs both the original
     and the LQ of the code with arguements specified. Only necessary arguments are dataframe and code. """
     #Create a cmap that's more appropriate for displaying LQ
-    lqmap = shift_colormap('YlOrBr', midpoint=1.0)
-    
-    #Remove 0 in LQ; more buckets for other values
-    col_list = dataframe.columns.tolist()
-    lqlist = [lq for lq in col_list if 'LQ_' in lq]
-    for lq in lqlist:
-        dataframe[lq].replace(0,'NaN')
         
     #start graphing
-        fig, axs = matplotlib.pyplot.subplots(1,2, figsize=(20,10))
-        axs[0].set_title(code)
-        axs[1].set_title('LQ_'+ code)
-        return dataframe.plot(column= code, cmap=cmap, k=k, scheme=scheme, figsize = (10,10), legend=True, ax=axs[0]), dataframe.plot(column= 'LQ_' + code, cmap=lqmap, k=k, scheme=scheme, figsize = (10,10), legend=True, ax=axs[1])
+    fig, axs = matplotlib.pyplot.subplots(1,2, figsize=(20,10))
+    axs[0].set_title(code)
+    axs[1].set_title('LQ_'+ code)
+    return dataframe.plot(column= code, cmap=cmap, k=k, scheme=scheme, figsize = (10,10), legend=True, ax=axs[0]), dataframe.plot(column= 'LQ_' + code, cmap=lqmap, k=k, scheme=scheme, figsize = (10,10), legend=True, ax=axs[1])
 
 
 # %%
